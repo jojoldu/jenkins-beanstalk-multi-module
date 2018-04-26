e6f4add7e47c4b53c17e7dafaf1da9a3b5bafd17
jojoldu <jojoldu@gmail.com>
2018-04-27 07:41:54 +0900

    Initial commit

---
+++ src/main/java/com/jojoldu/aws/beanstalk/JenkinsBeanstalkMultiModuleApplication.java
@@ -0,0 +1,12 @@
+package com.jojoldu.aws.beanstalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsBeanstalkMultiModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsBeanstalkMultiModuleApplication.class, args);
+    }
+}
---
+++ src/test/java/com/jojoldu/aws/beanstalk/JenkinsBeanstalkMultiModuleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jojoldu.aws.beanstalk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JenkinsBeanstalkMultiModuleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
