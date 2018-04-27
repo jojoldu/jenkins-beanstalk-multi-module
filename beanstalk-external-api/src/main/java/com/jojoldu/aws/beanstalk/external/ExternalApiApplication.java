a0288e641b989d8aecadcb6070b1a6682181333b
jojoldu <jojoldu@gmail.com>
2018-04-27 20:41:29 +0900

    jenkins 설치 완료

--- beanstalk-external-api/src/main/java/com/jojoldu/aws/beanstalk/external/ExternalApiApplication.java
+++ beanstalk-external-api/src/main/java/com/jojoldu/aws/beanstalk/external/ExternalApiApplication.java
@@ -2,11 +2,19 @@ package com.jojoldu.aws.beanstalk.external;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ExternalApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ExternalApiApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String index () {
+        return "Hello External API";
     }
 }
